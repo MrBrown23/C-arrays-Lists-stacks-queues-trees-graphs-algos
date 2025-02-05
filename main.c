7f80ee91e4ce6df30cca9e649b095bd7f86796f8
Gzuz <MRRbrown@protonmail.com>
2025-02-05 17:16:41 +0100

    Modifing main.c to review arrays

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	printf("Test\n");
+	double prices[] = {1.2, 5.3, 4.7, 88.5, 19.6, 2222.44};
+	for(int i=0; i<6;i++){
+		printf("%.2lf\n", prices[i]);
+	}
 	return 0;
 }
