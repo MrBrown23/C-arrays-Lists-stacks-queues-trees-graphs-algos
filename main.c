d4df83f48a1705f1e9b86de7889a144ecaf0f53f
Gzuz <MRRbrown@protonmail.com>
2025-02-05 17:44:03 +0100

    The second version after modifiyng main.c, here we use sizeof() instead of a hardcoding a number.

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	double prices[] = {1.2, 5.3, 4.7, 88.5, 19.6, 2222.44};
-	for(int i=0; i<6;i++){
-		printf("%.2lf\n", prices[i]);
+	double prices[][3] = {{1.2, 5.3, 4.7}, {88.5, 19.6, 2222.44}};
+	int rows =  sizeof(prices) / sizeof(prices[0]);
+	int cols = sizeof(prices[0]) / sizeof(prices[0][0]);
+	for(int i=0; i < rows;i++){
+		for(int j=0; j< cols; j++)
+			printf("%.2lf\t", prices[i][j]);
+		printf("\n");
 	}
+	
 	return 0;
 }
