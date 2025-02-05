43411b5ef4bf4e2fd6debc96c4cbd2079a2a6267
Gzuz <MRRbrown@protonmail.com>
2025-02-05 21:23:09 +0100

    The second version after modifiyng main.c, here we use sizeof() instead of a hardcoding a number.

---
+++ pointers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // int var = 21;
+    // int * ptr = &var;
+    // *ptr += 2;
+    // printf("%d\n", var); 
+    // printf("%p\n", &var);
+    // printf("%p\n", ptr);
+    // int a=5;
+    // int b=3;
+    // printf("a = %d\n", a);
+    // printf("b = %d\n", b);
+    // swap(&a,&b);
+    // printf("a = %d\n", a);
+    // printf("b = %d\n", b);
+    int *a;
+    int length;
+    printf("Enter the length of your array: ");
+    scanf("%d", &length);
+
+    a = malloc(length * sizeof(length));
+    for(int i=0; i<length; i++)
+        a[i] = i;
+    for(int i=0; i<length; i++)
+        printf("a[%d] = %d\n",i, a[i]);
+    free(a);
+    return 0;
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
