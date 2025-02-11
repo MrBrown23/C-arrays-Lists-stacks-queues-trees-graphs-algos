6e151381d077041587533d8f256a3ba126a3e5a7
Gzuz <MRRbrown@protonmail.com>
2025-02-11 23:49:56 +0100

    Adding the destroy_graph function.

--- graphs.c
+++ graphs.c
@@ -3,7 +3,7 @@
 #include "graph_adj_mat.h"
 
 struct mygraph{
-    int numnode;
+    int numnodes;
     bool **edges;
 };
 
@@ -13,13 +13,42 @@ graph *create_graph(int numnodes){
         return NULL;
     }
 
-    g->numnode = numnodes;
+    g->numnodes = numnodes;
 
-    
+    g->edges = calloc(sizeof(bool*), g->numnodes);
+    if(g->edges == NULL){
+        free(g);
+        return NULL;
+    }
+
+    for (int i = 0; i < g->numnodes; i++)
+    {
+        g->edges[i] = calloc(sizeof(bool), g->numnodes);
+        if(g->edges[i] == NULL){
+            destroy_graph(g);
+            return NULL;
+        }
+    }
+
+    return g;
+
 }
+
+
 void destroy_graph(graph *g){
+    if(g->edges == NULL){
+        return;
+    }
+    for(int i=0; i< g->numnodes; i++){
+        if(g->edges[i] != NULL){
+            free(g->edges[i]);
+        }
+    }
 
+    free(g->edges);
 }
+
+
 void print_graph(graph *g){
 
 }
