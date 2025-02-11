861c0244d8bc032bc5302442fa5f1b677acdb033
Gzuz <MRRbrown@protonmail.com>
2025-02-11 18:29:19 +0100

    Modifying lists_review.c file.

--- lists_review.c
+++ lists_review.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
-    int data;
+typedef struct node
+{
+    int value;
     struct node* next;
-    
 }node;
 
-
-void displaylist(node *head){
-    node *p;
-    p = head;
+void print_list(node* head){
+    node *p = head;
     while(p!=NULL){
-        printf("%d->", p->data);
+        printf("%d->", p->value);
         p = p->next;
     }
     printf("NULL\n");
 }
 
+node *create_list(){
+    node* head = NULL;
+    node* temp = NULL;
+    node* p = NULL;
+    char choice;
+    do{
+        temp = (node*)malloc(sizeof(node));
+        printf("Enter a new element to the list: ");
+        scanf("%d", &(temp->value));
+        if(head == NULL){
+            head = temp;
+        }
+        else{
+            p = head;
+            while(p->next!=NULL){
+                p = p->next;
+            }
+            p->next = temp; 
+        }
+        
+        printf("Do you want to continue (Y/N)? ");
+        scanf(" %c", &choice);
+    }while (choice == 'Y' || choice == 'y');
+    return head;
+}
+
+
 int main(){
-    node head;
-    
-    head.data = 15;
-    head.next = malloc(sizeof(node));
-    head.next->data = 22;
-    head.next->next = NULL;
-    displaylist(&head);
-    free(head.next);
+    node* head;
+    head = create_list();
+    print_list(head);
     return 0;
+
 }
