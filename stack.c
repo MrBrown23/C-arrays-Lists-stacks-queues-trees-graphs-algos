a9083082de58fafbf31511c85d1ea4919c6516f3
Gzuz <MRRbrown@protonmail.com>
2025-02-12 23:52:00 +0100

    Adding the push function.

--- stack.c
+++ stack.c
@@ -1,39 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <stdbool.h>
 
 #define STACK_LENGTH 5
 #define EMPTY (-1)
-#define STACK_EMPTY INT_MIN
 
-int my_stack[STACK_LENGTH];
+int mystack[STACK_LENGTH];
+
 int top = EMPTY;
 
 bool push(int value){
-    if(top>=STACK_LENGTH-1){
-    return false;
-    }
+    if(top >= STACK_LENGTH - 1)
+        return false;
     top++;
-    my_stack[top] = value;
+    mystack[top] = value;
     return true;
 }
 
-int pop(){
-    if(top == EMPTY)
-        return STACK_EMPTY;
-    int result = my_stack[top];
-    top--;
-    return result;
-}
-
 int main(){
-    push(25);  
-    push(13);
-    push(50);
-    int t;
-    while((t= pop())!= STACK_EMPTY){
-        printf("%d\n", t);
-    } 
     return 0;
 }
