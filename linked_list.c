40978ca1954192ef68ee77c0d041e6851c3a676d
Gzuz <MRRbrown@protonmail.com>
2025-02-13 23:33:28 +0100

    Adding create_list function.

---
+++ linked_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int value;
+    struct Node *next;
+}Node;
+
+typedef struct Node node;
+
+node *create_list(){
+    node *head = NULL;
+    node *temp = NULL;
+    node *p = NULL;
+    char choice;
+    do{
+        temp = malloc(sizeof(node));
+        printf("Enter a new element into the list: ");
+        scanf("%d",&(temp->value));
+        temp->next = NULL;
+        if(head == NULL){
+            head = temp;
+        }
+        else{
+            p = head;
+            while (p->next != NULL)
+            {
+               p = p->next;
+            }
+            p->next = temp;
+            
+        }
+        printf("Do you want to continue (Y/N)? ");
+        scanf(" %c", &choice);
+    }while(choice == 'Y' || choice == 'y');
+
+    return head;
+}
+
