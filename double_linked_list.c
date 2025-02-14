b9f77f6d3e3cb44b36c19984be78553e8f2e31a5
Gzuz <MRRbrown@protonmail.com>
2025-02-14 22:46:22 +0100

    Adding create_list function.

--- double_linked_list.c
+++ double_linked_list.c
@@ -3,7 +3,7 @@
 
 typedef struct node{
     int value;
-    node *next;
+    struct node *next;
 }node;
 
 void print_list(node *head){
@@ -15,6 +15,38 @@ void print_list(node *head){
     printf("NULL\n");
 }
 
+node *create_list(){
+    node *head = NULL;
+    node *temp = NULL;
+    node *p = NULL;
+    char choice;
+    do
+    {
+        temp = malloc(sizeof(node));
+        printf("Enter a new element to the list: ");
+        scanf("%d", &(temp->value));
+        temp->next =NULL;
+        if(head == NULL){
+            head = temp;
+        }
+        else{
+            p = head;
+            while(p->next != NULL)
+            {
+                p = p->next;
+            }
+            p->next = temp;
+        }
+        printf("Do you want to continue (Y/N)? ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+
+    return head;
+    
+}
+
 int main(){
+    node *head = create_list();
+    print_list(head);
     return 0;
 }
