70676d36734b17844429b9061bce779341bfa6be
Gzuz <MRRbrown@protonmail.com>
2025-02-17 23:17:05 +0100

    Adding print_tree function.

--- trees.c
+++ trees.c
@@ -1,98 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-typedef struct tree_node
-{
+typedef struct tree_node{
     int value;
-    struct tree_node* left;
-    struct tree_node* right;
+    struct tree_node *left;
+    struct tree_node *right;
 }tree_node;
 
-
-tree_node* create_node(int value)
-{
-    tree_node *root = malloc(sizeof(tree_node));
-    root->value = value;
-    root->left = NULL;
-    root->right = NULL;
+void print_tree(tree_node *root){
+    if(root == NULL){
+        printf("=================<Empty>====================\n");
+        return;
+    }
+    printf("value = %d\n", root->value);
+    printf("left of %d\n", root->value);
+    print_tree(root->left);
+    printf("right of %d\n", root->value);
+    print_tree(root->right);
 }
 
-void print_dfs(tree_node *root)
-{
-    tree_node* tree_stack[20];
-    int tree_stack_num = 0;
-    tree_stack[tree_stack_num] = root;
-    tree_stack_num++;
-    while (tree_stack_num > 0)
-    {
-       tree_node *current_node = tree_stack[tree_stack_num-1];
-       tree_stack_num--;
+tree_node *create_node(int value){
+    tree_node *node = malloc(sizeof(tree_node));
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
 
-       printf("%d ", current_node->value);
-       
-       if(current_node->right != NULL){
-        tree_stack[tree_stack_num] = current_node->right;
-        tree_stack_num++;
-       }
+int main(){
+    tree_node *root = create_node(0);
+    tree_node *n1 = create_node(1);
+    tree_node *n2 = create_node(2);
+    tree_node *n3 = create_node(3);
+    tree_node *n4 = create_node(4);
+    tree_node *n5 = create_node(5);
+    tree_node *n6 = create_node(6);
 
-       
-       if(current_node->left != NULL){
-        tree_stack[tree_stack_num] = current_node->left;
-        tree_stack_num++;
-       }
-    }
-    printf("\n");
-    
+    root->left = n1;
+    root->right = n2;
+
+    n1->left = n3;
+    n1->right = n4;
+
+    n2->left = n5;
+    n2->right = n6;
+
    
-}
 
-void print_bfs(tree_node *root)
-{
-    tree_node *queue_tree[20];
-    int size = 0;
-    queue_tree[size] = root;
-    size++;
-    while (size > 0)
-    {
-        tree_node *current_node = queue_tree[size-1];
-        size--;
-        printf("%d ", current_node->value);
-        if(current_node->left != NULL){
-            queue_tree[size] = current_node->left;
-            size++;
-           }
-           
-        if(current_node->right != NULL){
-            queue_tree[size] = current_node->right;
-            size++;
-           }
-    
-           
-           
-    }
-    printf("\n");
-}
-
-
-int main()
-{
-    tree_node* root = malloc(sizeof(tree_node));
-
-    root->value = 0;
-    root->left = create_node(1);
-    root->right = create_node(2);
-    root->left->left = create_node(3);
-    root->left->right = create_node(4);
-    root->right->left = create_node(5);
-    root->right->right = create_node(6);
-    print_bfs(root);
-
-    // printf("Root: %d\n", root->value);
-    // printf("Left child: %d\n", root->left->value);
-    // printf("Right child: %d\n", root->right->value);
-
-    print_dfs(root);
+    print_tree(root);
 
     return 0;
+       
+
 }
