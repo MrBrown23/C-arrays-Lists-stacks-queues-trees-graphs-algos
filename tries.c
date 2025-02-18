95c4e46ddeb42d844519121b2b17da59b2e49652
Gzuz <MRRbrown@protonmail.com>
2025-02-18 23:44:47 +0100

    Adding trieinsert function.

--- tries.c
+++ tries.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define NUM_CHARS 256
 
@@ -21,6 +22,33 @@ trienode *createnode()
 
     newnode->terminal = false;
     return newnode;
+}
+
+bool trieinsert(trienode **root, char *signedtext)
+{
+    if(*root == NULL)
+    {
+        *root = createnode();
+    }
+    trienode *temp = *root;
+    unsigned char *text = (unsigned char *)signedtext;
+    int length = strlen(signedtext);
+    for(int i=0; i<length; i++)
+    {
+        if(temp->children[text[i]] == NULL){
+            temp->children[text[i]] = createnode();
+        }
+        temp = temp->children[text[i]];
+    }
+    if(temp->terminal){
+        return false;
+    }
+    else 
+    {
+       temp->terminal = true;
+       return true;
+    }
+    
 }
 
 int main()
